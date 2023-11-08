33c1d77fdd2d2c748575b4e9a7a53499fa86cc58
Njoe00 <nick.james.joe@gmail.com>
2023-11-07 20:00:14 -0800

    adding footer with styling and buttons

---
+++ src/app/footer/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+const socialMediaButtonArray = [
+  {
+    link: "https://www.linkedin.com/in/nicholas-joe/",
+    default: "/linkedin_white.png",
+    highlighted: "/linkedin_blue.png",
+  },
+  {
+    link: "https://github.com/Njoe00",
+    default: "/github_white.png",
+    highlighted: "/github_blue.png",
+  },
+];
+
+export default function Footer() {
+  return (
+    <div className="flex flex-row items-center  bg-[#2d2e32] -mx-40 justify-around py-12">
+      <span className="text-xl text-white font-bold">
+        Copyright © 2023. All rights are reserved{" "}
+      </span>
+      <span>
+        <span className="flex-row flex space-x-2">
+          {socialMediaButtonArray.map((item, index) => {
+            const [toggleOnHover, setToggleOnHover] = useState(false);
+
+            const handleMouseEnter = () => {
+              setToggleOnHover(true);
+            };
+
+            const handleMouseLeave = () => {
+              setToggleOnHover(false);
+            };
+
+            return (
+              <a href={item.link} key={index}>
+                <Image
+                  className="cursor-pointer"
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  src={`${toggleOnHover ? item.highlighted : item.default}`}
+                  height={30}
+                  width={30}
+                  quality={100}
+                  alt="other"
+                />
+              </a>
+            );
+          })}
+        </span>
+      </span>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from "./header/page";
 import MainCard from "./mainCard/page";
 import About from "./about/page";
 import Contact from "./contact/page";
+import Footer from "./footer/page";
 export default function Home() {
   return (
     <main className="bg-[#f9f9f9] text-white h-screen w-screen box-border">
@@ -11,6 +12,7 @@ export default function Home() {
       <MainCard />
       <About />
       <Contact />
+      <Footer />
     </main>
   );
 }
