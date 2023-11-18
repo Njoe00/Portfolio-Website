78d5ac37d6ebf2380bb367fca2977b258b2bc130
Njoe00 <nick.james.joe@gmail.com>
2023-11-17 18:26:22 -0800

    adding styling to footer for smaller sizes

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -15,17 +15,17 @@ const socialMediaButtonArray = [
 
 export default function Footer() {
   return (
-    <div className="flex flex-row items-center bg-[#2d2e32] justify-around py-12">
-      <span className="text-xl text-white font-bold">
+    <div className="flex flex-row xs:flex-col sm:flex-col xs:justify-center sm:justify-center items-center space-y-10 bg-[#2d2e32] justify-around xs:py-14 sm:py-10">
+      <h1 className="text-xl text-white font-bold xs:text-center sm:text-center flex">
         Copyright © 2023. All rights are reserved{" "}
-      </span>
-      <span>
-        <span className="flex-row flex space-x-4">
+      </h1>
+      <div>
+        <div className="flex-row flex gap-4">
           {socialMediaButtonArray.map((item, index) => {
             return <SocialMediaButtonsFooter item={item} key={index} />;
           })}
-        </span>
-      </span>
+        </div>
+      </div>
     </div>
   );
 }
